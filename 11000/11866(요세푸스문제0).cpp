85a4fe403c1e6844492fd117ecc818d780aa028b
Jeong <JeongYK92@gmail.com>
2020-06-25 19:44:04 +0900

    추가

---
+++ 11000/11866(요세푸스문제0).cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+  ios_base::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  queue<int> q;
+  int n, k;
+  cin >> n >> k;
+
+  for (int i = 1; i <= n; i++)
+  {
+    q.push(i);
+  }
+
+  cout << "<";
+
+  for (int i = 1; i < n; i++)
+  {
+    for (int j = 0; j < k - 1; j++)
+    {
+      int tmp = q.front();
+      q.pop();
+      q.push(tmp);
+    }
+    cout << q.front() << ", ";
+    q.pop();
+  }
+
+  cout << q.front() << ">\n";
+  return 0;
+}
